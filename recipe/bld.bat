743844630c79866830e87bdb9986a6d1a259bf1b
Balazs Nagy <bnagy@nvidia.com>
2026-04-10 15:22:07 +0200

    Scripts: Adding Python free-threading support
    For instance, 314t suggests Python version 3.14 with free-threading.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,13 @@ if errorlevel 1 exit /b 1
 :: Install the Python wheel
 %PYTHON% -m pip install .\nvidia_nvcomp_cu%major_version%-%PKG_VERSION%-py3-none-win_amd64.whl --no-deps
 
+:: Query whether the current Python was configured with GIL disabled
+for /f "delims=" %%i in ('%PYTHON% -c "import sysconfig; print(int(bool(sysconfig.get_config_var(\"Py_GIL_DISABLED\"))))"') do set "py_free_threaded=%%i"
+set "PY_VER_t=%PY_VER:.=%"
+if "!py_free_threaded!" == "1" (
+    set "PY_VER_t=!PY_VER_t!t"
+)
+
 :: Exit on error
 if errorlevel 1 exit /b 1
 
@@ -25,9 +32,13 @@ if errorlevel 1 exit /b 1
 for %%f in (%SP_DIR%\nvidia\nvcomp\nvcomp_impl.cp*-win_amd64.pyd) do (
     for /f "tokens=1 delims=-" %%i in ("%%~nf") do (
         set "temp=%%i"
+
+        :: Python version with optional suffix t (indicating free-threadedness), e.g., 314t, or 314
         set "file_py_ver=!temp:*.cp=!"
-        if not "!file_py_ver!" == "%PY_VER:.=%" (
-            echo Deleting %%f as its Python version ^(!file_py_ver!^) does not match %PY_VER%
+
+        :: Delete the file if the Python version or the free-threadedness does not match
+        if not "!file_py_ver!" == "!PY_VER_t!" (
+            echo Deleting %%f as its Python version ^(!file_py_ver!^) does not match !PY_VER_t!
             del "%%f"
         )
     )
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,15 +4,20 @@ cuda_major=$(echo $cuda_compiler_version | cut -d. -f1)
 
 $PYTHON -m pip install ./nvidia_nvcomp_cu$cuda_major-$PKG_VERSION-*.whl --no-deps
 
+# Query free-threadedness
+py_free_threaded=$($PYTHON -c "import sysconfig; print(int(bool(sysconfig.get_config_var('Py_GIL_DISABLED'))))")
+PY_VER_t=$(echo $PY_VER | tr -d '.')
+[ "$py_free_threaded" -eq 1 ] && PY_VER_t="${PY_VER_t}t"
+
 rm -rvf $SP_DIR/nvidia/nvcomp/include
 rm -rvf $SP_DIR/nvidia/nvcomp/libnvcomp.so.*
 
 # Loop through files and delete those whose Python version does not match $PY_VER
 for file in $SP_DIR/nvidia/nvcomp/nvcomp_impl.cpython-*-linux-gnu.so; do
-    # Extract the Python version from the file name (312 for Python 3.12, etc.)
-    file_py_ver=$(echo "$file" | sed -n 's/.*\.cpython-\([0-9][0-9]*\)-.*-linux-gnu\.so/\1/p')
-    if [[ "$file_py_ver" != "$(echo $PY_VER | tr -d '.')" ]]; then
-        echo "Deleting $file as its Python version ($file_py_ver) does not match $PY_VER"
+    # Extract the Python version from the file name (312 for Python 3.12, 314t for Python 3.14t, etc.)
+    file_py_ver=$(echo "$file" | sed -n 's/.*\.cpython-\([0-9][0-9]*t*\)-.*-linux-gnu\.so/\1/p')
+    if [[ "$file_py_ver" != "$PY_VER_t" ]]; then
+        echo "Deleting $file as its Python version ($file_py_ver) does not match $PY_VER_t"
         rm "$file"
     fi
 done
